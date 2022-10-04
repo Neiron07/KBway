c01d0a24692418f572a3e42410aa6dd688997c4f
Neiron07 <neiron@vvv.cash>
2022-10-04 22:03:56 +0600

    cors

--- src/server/server.js
+++ src/server/server.js
@@ -4,12 +4,16 @@ import router from './routes/user.js';
 import authRouter from './routes/auth.js';
 import * as dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+import morgan from 'morgan';
 dotenv.config();
 const url = process.env.MONGO_URI;
 const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 app.use('/api', router);
 app.use('/api/auth', authRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup());
